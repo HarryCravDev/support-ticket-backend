c38382ec02c395425fc12c67d3654debbccf6511
Harry Craven <harrycraven.dev@gmail.com>
2022-07-07 16:38:55 +0100

    feature/login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Post, Route } from "tsoa";
+import { Body, Controller, Get, Post, Route, Security } from "tsoa";
 import userService from "../services/user.service";
 import IGenericSuccessResponse from "../types/IGenericSuccessResponse";
 import IGenericFailureResponse from "../types/IGenericFailureResponse";
 import IUser from "../types/IUser";
+import IUserLogin from "../types/IUserLogin";
+import { generateToken } from "../authentication/generateToken";
 
 @Route("v1/api/users")
 export class UsersController extends Controller {
@@ -26,5 +28,42 @@ export class UsersController extends Controller {
 
 			return { success: false, message: error.message };
 		}
+	}
+
+	@Post("login")
+	public async login(
+		@Body() user: IUserLogin
+	): Promise<IGenericSuccessResponse | IGenericFailureResponse> {
+		try {
+			this.setStatus(201);
+			const res = await userService.login(user);
+
+			return {
+				success: true,
+				message: "Logged in successful.",
+				data: {
+					...res,
+					token: generateToken({ id: res._id.toLocaleString() }),
+				},
+			};
+		} catch (error: any) {
+			this.setStatus(500);
+			let message = error.message;
+			console.log("error from harry");
+
+			// Todo - Add error catches
+			if (error.message.includes("User already exists")) {
+				this.setStatus(400);
+				message = "User already exists.";
+			}
+
+			return { success: false, message: error.message };
+		}
+	}
+
+	@Security("jwt")
+	@Post("foo")
+	public async foo(): Promise<any> {
+		return { success: true, message: "Foo" };
 	}
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,7 @@ import IUser from "../types/IUser";
 import User from "../models/User.model";
 import bcrypt from "bcryptjs";
 import { Types } from "mongoose";
+import IUserLogin from "../types/IUserLogin";
 
 interface IUserReturn {
 	_id: string | number | Types.ObjectId;
@@ -23,13 +24,37 @@ class UserService {
 		try {
 			const user = await User.create({
 				name: userData.name,
-				email: userData.email,
+				email: userData.email.toLowerCase(),
 				password: hashedPassword,
 			});
 
-			return { _id: user._id, name: user.name, email: user.email };
+			return {
+				_id: user._id,
+				name: user.name,
+				email: user.email,
+			};
 		} catch (error) {
 			throw new Error("Error creating user");
+		}
+	}
+
+	public async login({ email, password }: IUserLogin): Promise<IUserReturn> {
+		try {
+			const user = await User.findOne({ email: email.toLowerCase() });
+
+			if (!user) {
+				throw new Error("User not found");
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				throw new Error("Incorrect credentials");
+			}
+
+			return { _id: user._id, name: user.name, email: user.email };
+		} catch (error) {
+			throw new Error("Error logging in");
 		}
 	}
 }
