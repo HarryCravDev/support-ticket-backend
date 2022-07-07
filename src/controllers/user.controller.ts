38ddf0610c998e6b80045effd0dc191bebd7a25f
Harry Craven <harrycraven.dev@gmail.com>
2022-07-07 13:14:49 +0100

    feature/user-register-route-setup-and-user-model

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get, Route } from "tsoa";
+import { Body, Controller, Get, Post, Route } from "tsoa";
+import userService from "../services/user.service";
+import IGenericSuccessResponse from "../types/IGenericSuccessResponse";
+import IGenericFailureResponse from "../types/IGenericFailureResponse";
+import IUser from "../types/IUser";
 
-@Route("users")
+@Route("v1/api/users")
 export class UsersController extends Controller {
-	@Get()
-	public async test(): Promise<any> {
-		return { success: true, message: "Route working..." };
+	@Post("register")
+	public async createUser(
+		@Body() user: IUser
+	): Promise<IGenericSuccessResponse | IGenericFailureResponse> {
+		try {
+			const res = await userService.createUser(user);
+			this.setStatus(201);
+			return { success: true, message: "User created.", data: res };
+		} catch (error: any) {
+			this.setStatus(500);
+			let message = error.message;
+
+			// Todo - Add error catches
+			if (error.message.includes("User already exists")) {
+				this.setStatus(400);
+				message = "User already exists.";
+			}
+
+			return { success: false, message: error.message };
+		}
 	}
 }
