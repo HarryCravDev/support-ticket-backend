f63ab17a85ac92d6d23e4c933a0ef49503bfffdb
Harry Craven <harrycraven.dev@gmail.com>
2022-07-11 10:10:39 +0100

    feature/ticketing

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Path, Post, Route, Security } from "tsoa";
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Path,
+	Post,
+	Put,
+	Route,
+	Security,
+} from "tsoa";
 import ITicket from "../types/ITicket";
 import IGenericSuccessResponse from "../types/IGenericSuccessResponse";
 import IGenericFailureResponse from "../types/IGenericFailureResponse";
@@ -38,7 +48,12 @@ export class TicketController extends Controller {
 				data: res,
 			};
 		} catch (error: any) {
-			return { success: false, message: error.message };
+			this.setStatus(500);
+			let message = error.message;
+
+			if (error.message.includes("User not found.")) this.setStatus(404);
+
+			return { success: false, message: message };
 		}
 	}
 
@@ -57,8 +72,70 @@ export class TicketController extends Controller {
 				data: res,
 			};
 		} catch (error: any) {
+			this.setStatus(500);
+			let message = error.message;
+
+			if (error.message.includes("User not found.")) this.setStatus(404);
+
 			console.log("Error: ", error);
-			return { success: false, message: error.message };
+			return { success: false, message: message };
+		}
+	}
+
+	// @Security("jwt")
+	@Put("{ticketId}")
+	public async updateTicket(
+		@Path() ticketId: string,
+		@Body() ticketData: any
+	): Promise<IGenericSuccessResponse | IGenericFailureResponse> {
+		this.setStatus(201);
+		try {
+			const res = await ticketService.updateTicket(ticketId, ticketData);
+
+			return {
+				success: true,
+				message: `Ticket ${ticketId} updated.`,
+				data: res,
+			};
+		} catch (error: any) {
+			this.setStatus(500);
+			let message = error.message;
+
+			if (error.message.includes("Ticket not found.")) this.setStatus(404);
+
+			if (error.message.includes("You can't update a ticket"))
+				this.setStatus(401);
+
+			console.log("Error: ", error);
+			return { success: false, message: message };
+		}
+	}
+
+	// @Security("jwt")
+	@Delete("{ticketId}")
+	public async removeTicket(
+		@Path() ticketId: string,
+		@Body() ticketData: any
+	): Promise<IGenericSuccessResponse | IGenericFailureResponse> {
+		this.setStatus(201);
+		try {
+			await ticketService.removeTicket(ticketId, ticketData);
+
+			return {
+				success: true,
+				message: `Ticket ${ticketId} removed.`,
+			};
+		} catch (error: any) {
+			this.setStatus(500);
+			let message = error.message;
+
+			if (error.message.includes("Ticket not found.")) this.setStatus(404);
+
+			if (error.message.includes("You can't update a ticket"))
+				this.setStatus(401);
+
+			console.log("Error: ", error);
+			return { success: false, message: message };
 		}
 	}
 }
