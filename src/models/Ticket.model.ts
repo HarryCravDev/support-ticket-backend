95376665dec83b2e32d6528243f0c9fe7414d40d
Harry Craven <harrycraven.dev@gmail.com>
2022-07-15 12:34:03 +0100

    feature/adding-ticket-model

--- src/models/Ticket.model.ts
+++ src/models/Ticket.model.ts
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
-const ticketSchema = new mongoose.Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		required: [true, "User is required"],
-		ref: "User",
+const ticketSchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: [true, "User is required"],
+			ref: "User",
+		},
+		product: {
+			type: String,
+			required: [true, "Product is required"],
+			enum: ["iPhone", "MacBook Air", "MacBook Pro", "iPad"],
+		},
+		description: {
+			type: String,
+			required: [true, "Description of the issue is required"],
+		},
+		status: {
+			type: String,
+			// required: [true, "Status is required"],
+			enum: ["open", "in progress", "closed"],
+			default: "open",
+		},
 	},
-	product: {
-		type: String,
-		required: [true, "Product is required"],
-		enum: ["iPhone", "MacBook Air", "MacBook Pro", "iPad"],
-	},
-	description: {
-		type: String,
-		required: [true, "Description of the issue is required"],
-	},
-	status: {
-		type: String,
-		required: [true, "Status is required"],
-		enum: ["open", "in progress", "closed"],
-		default: "open",
-	},
-});
+	{ timestamps: true }
+);
 
 export default mongoose.model("Ticket", ticketSchema);
