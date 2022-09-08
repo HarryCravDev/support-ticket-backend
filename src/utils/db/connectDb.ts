049ca5a19856adda7db043d9f1e7bcc67c9dedaa
Harry Craven <harrycraven.dev@gmail.com>
2022-09-08 22:04:30 +0100

    feature/data-seeder

--- src/utils/db/connectDb.ts
+++ src/utils/db/connectDb.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import config from "config";
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(config.get("db.mongo_url"));
-        console.log("MongoDB Connected: " + conn.connection.host);
-    } catch (error) {
-        console.error(`${error}`);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(config.get("db.mongo_url"));
+    console.log("MongoDB Connected: " + conn.connection.host);
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
